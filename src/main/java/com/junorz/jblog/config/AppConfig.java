7c9f60c7fb46af9612b920759ad6c92ee67b29e0
junorz <junvorz@gmail.com>
2019-06-20 16:23:38 +0900

    Add static contents cache feature

--- src/main/java/com/junorz/jblog/config/AppConfig.java
+++ src/main/java/com/junorz/jblog/config/AppConfig.java
@@ -1,11 +1,17 @@
 package com.junorz.jblog.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.CacheControl;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.junorz.jblog.context.bean.AppInfo;
 import com.junorz.jblog.context.orm.Repository;
@@ -32,6 +38,34 @@ public class AppConfig {
     @Scope(scopeName = "application")
     public AppInfo appInfo(Repository rep) {
         return new AppInfo(rep);
+    }
+    
+    @Configuration
+    @EnableWebMvc
+    public static class MvcConfig implements WebMvcConfigurer {
+        
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+            
+            registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+            
+            registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+            
+            registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+            
+            registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/");
+        }
+        
     }
     
     
